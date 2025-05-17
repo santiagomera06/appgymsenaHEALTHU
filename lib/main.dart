8e161e796e7a6b54dc93bfc0b41c58925ba75780
santiagomera06 <166671029+santiagomera06@users.noreply.github.com>
2025-05-17 12:10:14 -0500

    eliminacion

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:healthu/routes/desafios_routes.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HEALTHU',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.grey[100],
+      ),
+      initialRoute: DesafiosRoutes.desafios,
+      onGenerateRoute: DesafiosRoutes.generateRoute,
+    );
+  }
+}
